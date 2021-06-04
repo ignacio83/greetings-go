639b41c7e8d4572a95dbaf3f7cf2696a82501729
Andre de Fontana Ignacio <ignacio83@gmail.com>
2021-06-04 13:20:41 -0300

    Better tests organization

--- grettings/grettings_test.go
+++ grettings/grettings_test.go
@@ -5,18 +5,20 @@ import (
 	"testing"
 )
 
-func TestShouldPrintGreetingForGolangWhenGolangIsInput(t *testing.T) {
-	const name = "Golang"
-	want := regexp.MustCompile(`\b` + name + `\b`)
-	got, err := Hello("Golang")
-	if !want.MatchString(got) || err != nil {
-		t.Fatalf(`Hello("Golang") = %q, %v, want match for %#q, nil`, got, err, want)
-	}
-}
+func TestHello(t *testing.T) {
+	t.Run("Should print greeting for Golang", func(t *testing.T) {
+		const name = "Golang"
+		want := regexp.MustCompile(`\b` + name + `\b`)
+		got, err := Hello("Golang")
+		if !want.MatchString(got) || err != nil {
+			t.Fatalf(`Hello("Golang") = %q, %v, want match for %#q, nil`, got, err, want)
+		}
+	})
 
-func TestShouldReturnErrorWhenNameIsEmpty(t *testing.T) {
-	got, err := Hello("")
-	if got != "" || err == nil {
-		t.Fatalf(`Hello("") = %q, %v, want "", error`, got, err)
-	}
+	t.Run("Should return error when name is empty", func(t *testing.T) {
+		got, err := Hello("")
+		if got != "" || err == nil {
+			t.Fatalf(`Hello("") = %q, %v, want "", error`, got, err)
+		}
+	})
 }
